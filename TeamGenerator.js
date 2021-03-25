e6245ad0a9905bb7446494157f298ae3868a493e
“MonkeyPotatoe” <enrico.bonadiman.sk@gmail.com>
2021-03-25 15:35:20 +0000

    added css and js

---
+++ TeamGenerator.js
@@ -0,0 +1,23 @@
+// 1. take imput field value and generate array with value lenght
+
+// const numberOfTeams = document.getElementbyId("generate-teams").value;
+
+// const teamsArray = new Array(parseInt(numberOfTeams));
+
+// 2. for the array lenght generate as many cards
+generate - teams.onclick = function () {
+  //   for (i = 0; i < teamsArray.length; i++) {
+  let newCard = document.createElement("div");
+  newCard.className = "card";
+  let newh2 = document.createElement("h2");
+  newh2.className = "cardh2";
+};
+// };
+
+// 3. Append the cards under container on.click of the button
+
+// 4. take imput field value (names) and create array
+
+// 5. for array lenght select random name and create li item
+
+// 6. append li item to random card (for loop in for loop) delete selected name from list
